34352d45bce46c8b076d046ea70f8d498ec2d17f
damilola oyeyipo <oyeyipo45@gmail.com>
2022-09-28 00:00:53 +0100

    add url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  BASE_URL :  process.env.REACT_APP_BASE_URL || ''
-}
+  BASE_URL: process.env.REACT_APP_BASE_URL || 'https://sov-backend.herokuapp.com/graphql',
+};
