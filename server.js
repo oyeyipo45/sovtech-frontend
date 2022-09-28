d515c47054e1109c2991ad0d6acf9a8ea9bf2df4
damilola oyeyipo <oyeyipo45@gmail.com>
2022-09-28 09:07:38 +0100

    fix routing

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path')
 
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
+const isProduction = process.env.ENVIRONMENT;
+
+console.log(isProduction, 'isProduction');
 
 app.use(express.static('build'));
+
+isProduction &&
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../sovtech-frontend/build', 'index.html'));
+  });
+
+
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
 });
